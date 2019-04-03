2a5a98274a6f6c0d3411bc9633a4267dec951b1c
Craig Beuerlein <beuerlcd@miamioh.edu>
2019-04-03 18:12:35 -0400

    Beggining work on submitbutton_click

--- Front-End/Appgregate/Appgregate/SubmitApp.aspx.cs
+++ Front-End/Appgregate/Appgregate/SubmitApp.aspx.cs
@@ -12,10 +12,32 @@ namespace Appgregate
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Don't Touch this 
+            //Don't Touch this
         }
         protected void SubmitButton_Click(object sender, EventArgs e)
         {
+          /**
+          SqlConnection con = new SqlConnection();
+          {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into tbllogin values(@N, @Des, @Org, @Plt, @Ver, @R)",con);
+            cmd.Parameters.AddWithValue("N",Name.Text);
+            cmd.Parameters.AddWithValue("Des",Description.Text);
+            cmd.Parameters.AddWithValue("Org",Organization.Text);
+            cmd.Parameters.AddWithValue("Plt",Platform.Text);
+            cmd.Parameters.AddWithValue("Ver",Version.Text);
+            cmd.Parameters.AddWithValue("R",Rating.Text);
+            cmd.ExecuteNonQuery();
+
+            Name.Text = "";
+            Description.Text = "";
+            Organization.Text = "";
+            Platform.Text = "";
+            Version.Text = "";
+            Rating.Text = "";
+            Name.Focus();
+          }
+          **/
             //Write SQL storage query right here, check for validations, show something on the Webpage if necessary
         }
     }
