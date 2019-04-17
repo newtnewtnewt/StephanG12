12839ba5648fc0db6e35032bcd8879d1ee6b1d4b
newtnewtnewt <42472588+newtnewtnewt@users.noreply.github.com>
2019-04-17 16:46:16 -0400

    Uploaded My Build Script and Instruction List

---
+++ Front-End/Build Script/BootProject.bat
@@ -0,0 +1,6 @@
+cd C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
+MSBuild.exe "C:\Users\newtn\OneDrive\Documents\GitHub\StephanG12\Front-End\Appgregate\Appgregate.sln"
+cd C:\Program Files (x86)\IIS Express
+start "" http://localhost:55173
+iisexpress /path:C:\Users\newtn\OneDrive\Documents\GitHub\StephanG12\Front-End\Appgregate\Appgregate /port:55173
+
