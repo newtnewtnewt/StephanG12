c9f919bc17147627fe6646c9f2ed25716fc61b74
Noah Dunn <42472588+newtnewtnewt@users.noreply.github.com>
2019-04-24 19:30:02 -0400

    Added Test File

---
+++ Front-End/Appgregate/Appgregate/NUnit Test/NUnitTesting.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Diagnostics;
+
+
+namespace Appgregate.NUnit_Test
+{
+    [TestFixture]
+    public class NUnitTester
+    {
+        //Grey Box Testing 
+        [Test]
+        public void appCommentsExists()
+        {
+            AppComments x = new AppComments(); //Create an instance of App Comments
+            Assert.That(x != null); // The page indeed exists
+            
+
+        }
+        [Test]
+        public void defaultExists()
+        {
+            _Default x = new _Default(); //Create an instance of Default Webpages
+            Assert.That(x != null);      //The page does exist
+
+        }
+        [Test]
+        public void moderateApps()
+        {
+            ModerateApps x = new ModerateApps();
+            Assert.That(x != null);
+        }
+        //White Box Testing
+        [Test]
+        public void defaultPageLoad() {
+            _Default x = new _Default();
+            Assert.AreEqual(x.Page_Load(new object(), new EventArgs()), true);
+
+
+        }
+        [Test]
+        public void appCommentsPageLoad()
+        {   
+            AppComments x = new AppComments();
+            Assert.AreEqual(x.Page_Load(new object(), new EventArgs()), true);
+
+
+        }
+        [Test]
+        public void moderateAppsPageLoad()
+        {
+            ModerateApps x = new ModerateApps();
+            Assert.AreEqual(x.Page_Load(new object(), new EventArgs()), true);
+
+
+        }
+
+
+
+
+
+    }
+}
